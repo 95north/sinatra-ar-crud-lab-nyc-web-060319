a8651f5dda7cc80f1414821f0d0d38bd6ff97fd9
Tori Sigler <mssigler@gmail.com>
2019-06-26 16:41:57 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,48 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  # get '/' do
+  # end
+
+  get '/articles/new' do   			# CREATE
+  	erb :new
   end
+
+  post '/articles' do    			# CREATE
+  	@article = Article.create(title: params[:title], content: params[:content])
+  	erb :index
+  end
+
+  get '/articles' do 				# READ - iterate through @articles, render all. 
+  	@articles = Article.all 		# WORKS
+  	erb :index
+  end
+
+
+  get '/articles/:id' do   			# READ  ( # show action)    #WORKS
+  	@article= Article.find(params[:id])
+
+  	erb :show 
+  end
+
+  get '/articles/:id/edit' do 		#UPDATE - request form WORKS
+  	@article =  Article.find(params[:id])
+  	erb :edit
+  	#redirect "/articles/#{article.id}"
+  end
+
+
+  patch '/articles/:id' do 			#UPDATE - action for the edit  WORKS 
+  	@article =  Article.find(params[:id])
+  	@article.update(params[:article])
+  	redirect "/articles/#{@article.id}" #Takes us back to the READ    
+  end
+
+
+  delete('/articles/:id') do 			#DESTROY
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect('/articles')
+  end
+
 end
--- db/migrate/20151201201114_create_articles.rb
+++ db/migrate/20151201201114_create_articles.rb
@@ -5,5 +5,12 @@ class CreateArticles < ActiveRecord::Migration[5.1]
       t.string :content
 
     end
+
+  Article.create(:title =>  "Tori", :content => "My special article is about nothing." )
+  Article.create(:title =>  "Victor", :content => "In Black Shirt" )
+  Article.create(:title =>  "Vika", :content => "My russian name" )
+  Article.create(:title =>  "Victoria", :content => "Full names.." )
+
   end
+
 end
